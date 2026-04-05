655f503c8d095a3a70373ac277b0a3d4a9104d51
Gabriel Raul Marini <48673227+grm27@users.noreply.github.com>
2026-04-05 17:38:27 +0200

    Add: valid-tree - submission-0

---
+++ Data Structures & Algorithms/valid-tree/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        
+        visited = [False] * n
+        def has_cycle(prev, curr_node):
+            if visited[curr_node]:
+                return False
+            
+            visited[curr_node] = True
+            for nei in graph[curr_node]:
+                if nei != prev and not has_cycle(curr_node, nei):
+                    return False
+            return True
+        
+        return has_cycle(-1, 0) and visited.count(True) == n
